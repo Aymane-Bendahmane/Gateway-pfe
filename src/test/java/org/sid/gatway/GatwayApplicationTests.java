f733b794371a71be9735968fbd2081cc58d588b1
Aymane <aymane.bendahmane14@gmail.com>
2021-12-06 00:43:26 +0100

    first commit

---
+++ src/test/java/org/sid/gatway/GatwayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sid.gatway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatwayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
